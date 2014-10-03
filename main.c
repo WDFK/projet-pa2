b06458666d642331b9ffa1ab29d9bf79156eacd9
Paul Chabaux <chabaux2u@fst-ats-315-l17.fst.site.univ-lorraine.fr>
2014-10-03 14:23:24 +0200

    ajout du main

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "structure.h"
+#include "pieces.h"
+
+int main(void)
+{
+  object_list L = NULL;
+  L = lirePiece();
+
+  //afficherPiece(L);
+
+  return 0;
+}
